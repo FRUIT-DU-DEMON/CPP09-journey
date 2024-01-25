5f78185fdc6540bbf44ff4f9c53048c0bda0f67d
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-01-25 15:51:37 +0100

    Module05/ex00 started

---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 15:37:34 by hlabouit          #+#    #+#             */
+/*   Updated: 2024/01/25 15:50:34 by hlabouit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<ctype.h>
+#include<cstdlib>
+#include<cmath>
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    std::cout<< "Module 05 started" << std::endl;
+}
