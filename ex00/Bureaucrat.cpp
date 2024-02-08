9beac4b24842a702922a474dddf13088fe8b27c5
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-08 00:01:44 +0000

    OCF members implemented

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,8 +6,48 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:58:47 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/07 23:23:55 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/08 00:00:56 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include"Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &primary)
+{
+    *this = primary;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &primary)
+{
+    if (this != &primary)
+        this->grade = primary.grade;
+    return (*this);
+}
+
+Bureaucrat::Bureaucrat(const std::string &which_name, int which_grade) : name(which_name), grade(which_grade) {}
+
+const std::string &Bureaucrat::getName() const
+{
+    return (this->name);
+}
+
+int Bureaucrat::getGrade() const
+{
+    return (this->grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+    this->grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    this->grade++;
+}
+
+
+
+Bureaucrat::~Bureaucrat() {}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 03:46:46 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/07 23:23:15 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/07 23:58:38 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -27,10 +27,10 @@ class Bureaucrat {
 		int grade;
 	public:
 		Bureaucrat();
-        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const std::string &which_name, int which_grade);
 		Bureaucrat(const Bureaucrat &primary);
 		Bureaucrat &operator=(const Bureaucrat &primary);
-		std::string getName() const;
+		const std::string &getName() const;
 		int getGrade() const;
         void incrementGrade();
         void decrementGrade();
