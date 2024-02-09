dc9aecf610868bc9ca4d6f64b9dfe7b8d608365a
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-09 22:24:02 +0000

    ex00/day05--->DONE

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:37:34 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/09 22:21:56 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/09 22:23:18 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -19,6 +19,7 @@ int main()
 		Bureaucrat brc("IVXX", 1);
 		std::cout << brc << std::endl;
 		brc.incrementGrade();
+		std::cout << brc << std::endl;//won't be executed because an excp's been thrown
 	}
 	catch(const std::exception &excp)//any exception derived from std::exception thrown get caught 
 	{
