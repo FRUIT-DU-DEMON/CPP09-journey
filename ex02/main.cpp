a956639d191817911ee6a28fef7de7056d6f6d23
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-25 01:45:58 +0100

    ex02/day06---->DONE

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 22:33:36 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/25 01:22:25 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/25 01:45:29 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ Base *generate(void)
         return (new B());
     if (random == 2)
         return (new C());
+    return (NULL);
 }
 
 void identify(Base* p)
@@ -31,20 +32,23 @@ void identify(Base* p)
     /*dynamic_cast only used for performing safe downcasting of pointers or references in polymorphic class
     where it converts a base class pointer or reference to a derived class pointer or reference*/
     if (dynamic_cast<A *>(p) != NULL)
-        std::cout<< "the actual type of the object pointed to by p is: A";
+        std::cout<< "the actual type of the object pointed to by p is: A" << std::endl;
     else if (dynamic_cast<B *>(p) != NULL)
-        std::cout<< "the actual type of the object pointed to by p is: B";
+        std::cout<< "the actual type of the object pointed to by p is: B" << std::endl;
     else if (dynamic_cast<C *>(p) != NULL)
-        std::cout<< "the actual type of the object pointed to by p is: C";
+        std::cout<< "the actual type of the object pointed to by p is: C" << std::endl;
     else
         std::cout<< "dynamic_cast failed to downcasts";
+    return;
 }
 
 void identify(Base& p)
 {
     try
     {
-        dynamic_cast<A &>(p);
+        Base &ref = dynamic_cast<A &>(p);
+        (void)ref;
+        std::cout<< "the actual type of the object pointed to by p is: A" << std::endl;
     }
     catch(const std::exception &excp)
     {
@@ -52,7 +56,9 @@ void identify(Base& p)
     }
     try
     {
-        dynamic_cast<B &>(p);
+        Base &ref = dynamic_cast<B &>(p);
+        (void)ref;
+        std::cout<< "the actual type of the object pointed to by p is: B" << std::endl;
     }
     catch(const std::exception &excp)
     {
@@ -60,7 +66,9 @@ void identify(Base& p)
     }
     try
     {
-        dynamic_cast<C &>(p);
+        Base &ref = dynamic_cast<C &>(p);
+        (void)ref;
+        std::cout<< "the actual type of the object pointed to by p is: C" << std::endl;
     }
     catch(const std::exception &excp)
     {
@@ -70,8 +78,12 @@ void identify(Base& p)
 
 int main()
 {
-
     std::srand(std::time(NULL));
-    // std::cout<< p << std::endl;
-
+    Base *base_ptr = generate();
+    if (!base_ptr)
+        return (0);
+    identify(base_ptr);
+    identify(*base_ptr);
+    delete base_ptr;
+    return (0);
 }
