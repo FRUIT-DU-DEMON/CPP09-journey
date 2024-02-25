5a7739084300ac203dc55d6843fad7224974bf88
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-25 23:33:42 +0100

    ex00/day07---->DONE

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,24 +6,29 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 22:33:36 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/25 21:46:51 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/25 23:32:21 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"whatever.hpp"
 
-   int main( void ) {
+int main()
+{
     int a = 2;
     int b = 3;
+    
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    
     std::string c = "chaine1";
     std::string d = "chaine2";
+    
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    return 0;
+    
+    return (0);
 }
