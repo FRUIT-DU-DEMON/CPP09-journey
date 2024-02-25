4bc295939ebb50d59746b7449f1ddd180b90f814
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-25 01:19:14 +0100

    generate and identify functions implemented

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 22:33:36 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/24 23:35:31 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/25 01:18:20 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,42 @@
 #include"B.hpp"
 #include"C.hpp"
 
+Base *generate(void)
+{
+    int random = std::rand() % 3;
+    if (random == 0)
+        return (new A());
+    else if (random == 1)
+        return (new B());
+    if (random == 2)
+        return (new C());
+}
+
+void identify(Base* p)
+{
+    /*dynamic_cast only used for performing safe downcasting of pointers or references in polymorphic class
+    where it converts a base class pointer or reference to a derived class pointer or reference*/
+    if (dynamic_cast<A *>(p) != NULL)
+        std::cout<< "the actual type of the object pointed to by p is: A";
+    else if (dynamic_cast<B *>(p) != NULL)
+        std::cout<< "the actual type of the object pointed to by p is: B";
+    else if (dynamic_cast<C *>(p) != NULL)
+        std::cout<< "the actual type of the object pointed to by p is: C";
+    else
+        std::cout<< "the object pointed to by p doesn't match any type";
+}
+
+void identify(Base& p)
+{
+}
+
+
+
+
 int main()
 {
+
+    std::srand(std::time(NULL));
+    // std::cout<< p << std::endl;
 
 }
