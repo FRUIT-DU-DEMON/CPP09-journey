082521d7a8ef2c5f3216b68fc9f02b7e72c85011
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2024-02-28 21:32:16 +0100

    easyfind function template implemented

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Array.hpp                                          :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 22:33:08 by hlabouit          #+#    #+#             */
-/*   Updated: 2024/02/28 00:49:25 by hlabouit         ###   ########.fr       */
+/*   Updated: 2024/02/28 21:31:46 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_H
-# define ARRAY_H
+#ifndef EASYFIND_H
+# define EASYFIND_H
 
 #include<fstream>
 #include<iostream>
@@ -21,6 +21,24 @@
 #include<cstdlib>
 #include<cmath>
 #include<sstream>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+
+template <typename T>
+void easyfind(T &container, int to_find)
+{
+    typename T::iterator itr;
+    for (itr = container.begin(); itr < container.end(); itr++)
+    {
+        if (*itr == to_find)
+        {
+            std::cout<< "value was found within the container: " << to_find << std::endl;
+            return;
+        }
+    }
+    throw ("value was NOT found within the container!");
+}
 
 
 #endif
